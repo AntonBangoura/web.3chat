e73257a4be6822102cbd6a4c037b92460bf1d08b
Anton Bangoura <bangouranton@gmail.com>
2022-08-17 15:04:46 +0200

    final

--- src/user.js
+++ src/user.js
@@ -1,8 +1,22 @@
 import GUN from 'gun';
 import 'gun/sea';
 import 'gun/axe';
-
+import { writable } from 'svelte/store';
 //Database
 
 export const db = GUN();
 
+// user
+
+export const user = db.user().recall({sessionStorage: true})
+
+
+//current username usename
+export const username = writable('')
+user.get('alias').on(v => username.set(v));
+
+db.on('auth', async(event => {
+    const alias = await user.get('alias');
+    username.set(alias)
+    console.log(`you are signed as ${alias}`)
+}))
